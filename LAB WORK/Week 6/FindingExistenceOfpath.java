c2c3b8771f9ac809329eb13f6e0b3cfe852f3ef9
Shivendrashukla345 <89094393+Shivendrashukla345@users.noreply.github.com>
2021-09-23 13:32:40 +0530

    Update FindingExistenceOfpath.java

--- LAB WORK/Week 6/FindingExistenceOfpath.java
+++ LAB WORK/Week 6/FindingExistenceOfpath.java
@@ -1,48 +1,84 @@
+// Java program to print DFS
+//mtraversal from a given given
+// graph
 import java.io.*;
 import java.util.*;
- 
-class GFG {
-    private static void FindCommonElemet(String[] arr1,
-                                         String[] arr2)
+  
+// This class represents a 
+// directed graph using adjacency
+// list representation
+class Graph {
+    private int V; // No. of vertices
+  
+    // Array  of lists for 
+    // Adjacency List Representation
+    private LinkedList<Integer> adj[];
+  
+    // Constructor
+    @SuppressWarnings("unchecked") Graph(int v)
     {
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j]) {
- 
-                    // add common elements
-                    set.add(arr1[i]);
-                    break;
-                }
-            }
-        }
-        for (String i : set) {
-            System.out.print(i + " ");
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+  
+    // Function to add an edge into the graph
+    void addEdge(int v, int w)
+    {
+        adj[v].add(w); // Add w to v's list.
+    }
+  
+    // A function used by DFS
+    void DFSUtil(int v, boolean visited[])
+    {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        System.out.print(v + " ");
+  
+        // Recur for all the vertices adjacent to this
+        // vertex
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) 
+        {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
         }
     }
- 
-    // main method
-    public static void main(String[] args)
+  
+    // The function to do DFS traversal.
+    // It uses recursive
+    // DFSUtil()
+    void DFS(int v)
     {
- 
-        // create Array 1
-        String[] arr1
-            = { "Article", "in", "Geeks", "for", "Geeks" };
- 
-        // create Array 2
-        String[] arr2 = { "Geeks", "for", "Geeks" };
- 
-        // print Array 1
-        System.out.println("Array 1: "
-                           + Arrays.toString(arr1));
- 
-        // print Array 2
-        System.out.println("Array 2: "
-                           + Arrays.toString(arr2));
- 
-        System.out.print("Common Elements: ");
- 
-        // Find the common elements
-        FindCommonElemet(arr1, arr2);
+        // Mark all the vertices as 
+        // not visited(set as
+        // false by default in java)
+        boolean visited[] = new boolean[V];
+  
+        // Call the recursive helper 
+        // function to print DFS
+        // traversal
+        DFSUtil(v, visited);
+    }
+  
+    // Driver Code
+    public static void main(String args[])
+    {
+        Graph g = new Graph(4);
+  
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+  
+        System.out.println(
+            "Following is Depth First Traversal "
+            + "(starting from vertex 2)");
+  
+        g.DFS(2);
     }
 }
